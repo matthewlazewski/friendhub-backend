7a7b270f146243b74498af8fb6722d7d40757a02
matthewlazewski <matthew.lazewski@gmail.com>
2020-09-17 15:24:57 -0500

    moved controller to api/v1

---
+++ app/controllers/api/v1/likes_controller.rb
@@ -0,0 +1,2 @@
+class API::V1::LikesController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :friends
-  resources :posts
-  resources :likes
-  resources :comments
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+      
+  namespace :api do
+    namespace :v1 do
+      resources :friends
+      resources :posts
+      resources :likes
+      resources :comments
+      resources :users
+    end 
+  end  
 end

f992746145ff07f636058802a8ed54bedc296e7c
matthewlazewski <matthew.lazewski@gmail.com>
2020-09-18 15:32:41 -0500

    created like controller

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -1,2 +1,21 @@
 class API::V1::LikesController < ApplicationController
+    def index 
+        likes = Post.find(params[:post_id])
+        render json: likes 
+    end
+
+    def create
+        like = current_user.likes.new(post_id: params[:post_id])
+        render json: like 
+    end
+
+    def destroy 
+        like = Like.find(params[:id])
+
+        if like.destroy 
+            render json: like 
+        else 
+            render json: like.errors, status: 404
+        end
+    end
 end

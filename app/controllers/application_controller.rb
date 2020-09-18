138cdf15487fd4dce14a26bf7d74a7d07cd5c950
matthewlazewski <matthew.lazewski@gmail.com>
2020-09-18 15:44:40 -0500

    added  current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::API 
+    def current_user
+        @user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+    
+    def logged_in
+        !!current_user
+    end
 end

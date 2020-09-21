84bb2f3598b7adbe28cfb57ea715417a400c1e3d
matthewlazewski <matthew.lazewski@gmail.com>
2020-09-20 19:29:14 -0500

    fixed routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-  get '/logged_in', to: 'sessions#is_logged_in?'
+  post '/login', to: 'api/v1/sessions#create'
+  delete '/logout', to: 'api/v1/sessions#destroy'
+  get '/logged_in', to: 'api/v1/sessions#is_logged_in?'
       
   namespace :api do
     namespace :v1 do

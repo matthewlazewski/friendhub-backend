44a8c9ebe5af10676d9214d59279a1ecec387f33
matthewlazewski <matthew.lazewski@gmail.com>
2020-09-20 19:44:29 -0500

    added comment and like controllers

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,57 @@
 class Api::V1::PostsController < ApplicationController
+    def index 
+        @posts = Post.order(created_at: :desc)
+        .where(:author_id => Friend.active_friendships(current_user))
+        .includes(:author, :tagged, {likes: [:user]}, {comments: [:author]})
+        .order("created_at DESC")
+    end
+
+    def create
+        @post = current_user.posts.new(post_params)
+        if params.has_key?(:user_id)
+            unless current_user.id == params[:user_id].to_i
+            @post.tagged_user = params[:user_id]
+            end
+        end
+
+        if @post.save
+                render :show
+            else
+                render json: @post.errors, status: 422
+        end
+    end
+
+    def show
+        @post = Post.find(params[:id])
+        if @post
+            render :show
+        else
+            render json: ["post not found"], status: 404
+        end
+    end
+
+    def update
+        @post = Post.find(params[:id])
+        if @post.update(post_params)
+            render :show
+        else
+            render json: @post.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+
+        if @post.destroy
+            render :show
+        else
+            render json: @post.errors.full_messages, status: 422
+        end
+    end
+
+    private
+    
+    def post_params
+        params.require(:post).permit(:body, :image)
+    end
 end

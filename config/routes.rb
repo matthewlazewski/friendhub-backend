c3d3614056955b28d20a076435f698a2163fbd03
matthewlazewski <matthew.lazewski@gmail.com>
2020-09-24 20:11:11 -0500

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :friends
-      resources :posts 
+      resources :posts do
+        resources :comments
+      end
       resources :comments
       resources :likes
       resources :users, only: [:create, :show, :index]

891f24dad3c567bdd2c0d8a4c1d04d2eb59eab9f
matthewlazewski <matthew.lazewski@gmail.com>
2020-12-23 16:36:35 -0600

    added likes to models

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
     validates :body, :user_id, presence: true
 
     belongs_to :user
-    has_many :likes
+    has_many :likes, dependent: :destroy
     has_many :users, through: :comments
     has_many :comments
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :friends
-      resources :posts
+      resources :posts do
+        resource :likes 
+      end
       resources :comments
       resources :likes
       resources :users, only: [:create, :show, :index]

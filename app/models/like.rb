5e4b88ba8182eeb556ee168baa13edc3c84d3e78
matthewlazewski <matthew.lazewski@gmail.com>
2020-12-31 10:03:26 -0600

    fixed like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,11 +2,6 @@ class Like < ApplicationRecord
     validates :post, :user, presence: true
     validates_uniqueness_of :post_id, :scope => [:user_id]
 
-    belongs_to :post,
-        primary_key: :id,
-        foreign_key: :post_id
-    
+    belongs_to :post
     belongs_to :user 
-        primary_key: :id,
-        foreign_key: :user_id
 end

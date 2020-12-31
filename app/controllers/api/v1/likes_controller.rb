d1158b8fe6c60d87a7317cbbb360f484ae064622
matthewlazewski <matthew.lazewski@gmail.com>
2020-12-31 08:36:20 -0600

    simplified like model

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::LikesController < ApplicationController
-    # before_action :find_post
 
     def index 
-        likes = Like.all
+        @likes = Post.find(params[:post_id]).likes
         render json: LikeSerializer.new(likes)
     end
 
     def create
-        if like.save
+        @like = current_user.likes.new(post_id: params[:post_id])
+        if @like.save
             render json:{ 
                 like: LikeSerializer.new(like)
             }
@@ -17,26 +17,5 @@ class Api::V1::LikesController < ApplicationController
                 errors: comment.errors.full_messages
             }
         end
-    end
-
-    def destroy 
-        like = Like.find(params[:id])
-
-        if like.destroy 
-            render json: like 
-        else 
-            render json: like.errors, status: 404
-        end
-    end
-
-    private 
-
-    def find_post
-        @post = Post.find(params[:post_id]) 
-    end
-
-    def already_liked?
-        Like.where(user_id: current_user.id, post: post_id)
-        params[:post_id].exists?
     end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,11 @@ class Like < ApplicationRecord
     validates :post, :user, presence: true
     validates_uniqueness_of :post_id, :scope => [:user_id]
 
-    belongs_to :post
+    belongs_to :post,
+        primary_key: :id,
+        foreign_key: :post_id
+    
     belongs_to :user 
+        primary_key: :id,
+        foreign_key: :user_id
 end
